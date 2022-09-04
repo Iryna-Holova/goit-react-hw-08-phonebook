b684bdbcef0449d0118a55efd95044b753d51eb9
Iryna Holova <gis.golova@gmail.com>
2022-09-04 18:57:33 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 const Home = () => {
-    return <div>Home</div>
+    return <h1 style={{
+    fontSize: '78px',
+    color: '#5584b9',
+    }}>
+        Store and manage your contacts in our PhoneBook
+    </h1>
 }
 
 export default Home;
